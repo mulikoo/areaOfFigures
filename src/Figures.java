c5e2d5d82a1ef9e9315002dbec14573c949e5f3a
muliko <kasnicolas06@gmail.com>
2026-06-15 13:58:30 +0300

    Готовое задание

--- src/Figures.java
+++ src/Figures.java
@@ -1,2 +1,40 @@
+import java.util.Scanner;
+
 public class Figures {
+    private final Scanner sc = new Scanner(System.in);
+
+    public void square() {
+        System.out.println("Выберите фигуру: квадрат, круг, прямоугольник");
+        String figureType = sc.nextLine().toLowerCase();
+        double squareFigure = 0;
+        switch (figureType) {
+            case "квадрат":
+                System.out.println("Введите длину стороны квадрата:");
+                double x = sc.nextDouble();
+                squareFigure = x * x;
+                System.out.println("Площадь квадрата = " + squareFigure);
+                break;
+
+            case "круг":
+                System.out.println("Введите радиус круга");
+                double y = sc.nextDouble();
+                squareFigure = Math.PI * y * y;
+                System.out.println("Площадь круга = " + squareFigure);
+                break;
+
+            case "прямоугольник":
+                System.out.println("Введите длину прямоугольника");
+                double z = sc.nextDouble();
+                System.out.println("Введите ширину прямоугольника");
+                double r = sc.nextDouble();
+                squareFigure = z * r;
+                System.out.println("Площадь прямоугольника = " + squareFigure);
+                break;
+
+            default:
+                System.out.println("Был введен не известный тип фигуры. Попробуйте еще раз. " +
+                        "Доступны: квадрат, круг, прямоугольник");
+        }
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        Figures figures = new Figures();
+        System.out.println("Поиск площади для фигур");
+        figures.square();
     }
 }
